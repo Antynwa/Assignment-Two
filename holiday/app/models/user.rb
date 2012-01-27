8dd2cca24fc0ea5bf8e440dba165a6145f81fb84
Antony Shimmin <a.shimmin@2009.ljmu.ac.uk>
2012-01-27 11:50:50 +0000

    2nd Final Iteration

--- holiday/app/models/user.rb
+++ holiday/app/models/user.rb
@@ -7,6 +7,10 @@ has_many :holidays
   before_save :prepare_password
 
   validates_presence_of :username
+  validates_presence_of :fname
+  validates_presence_of :sname
+
+
   validates_uniqueness_of :username, :email, :allow_blank => true
   validates_format_of :username, :with => /^[-\w\._@]+$/i, :allow_blank => true, :message => "should only contain letters, numbers, or .-_@"
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
